46f0520708fc9ff94b37ee1f0d0eaaa5ac9e9334
Idan.Shuraty <idanshuraty89@gmail.com>
2016-05-21 22:28:59 +0300

    Motorcycle class

--- B16 Ex03 Idan 201580990 Sagi 123456789/Ex03GarageLogic/Vehicle.cs
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03GarageLogic/Vehicle.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ex03GarageLogic
+namespace Ex03.GarageLogic
 {
     public class Vehicle
     {
         string m_ModelName;
-        string m_LicenseNum;
+        string m_LisenseNum;
         float m_Energy;
-        //List<Wheel> m_Wheels;
+        List<Wheel> m_WheelsList;
+
+   
+        public Vehicle(string i_ModelName, string i_LisenseNum, float i_Energy, List<Wheel> i_WeelsList)
+        {
+            m_ModelName = i_ModelName;
+            m_LisenseNum = i_LisenseNum;
+            m_Energy = i_Energy;
+            m_WheelsList = i_WeelsList;
+        }
+
+
     }
+
 }
