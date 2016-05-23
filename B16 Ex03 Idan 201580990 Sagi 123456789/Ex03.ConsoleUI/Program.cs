b3faa065a8d97fb519f90c4fd2fa4fb155f61348
sagiGL <sagigleizer@gmail.com>
2016-05-23 18:52:25 +0300

    added few classes

---
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        public void main()
+        {
+            GarageManager newGarageManager = new GarageManager();
+            Console.WriteLine("Hello dear custumer and thanks for coming to our humble garage. \n Please choose one of the following options");
+        }
+    }
+}
