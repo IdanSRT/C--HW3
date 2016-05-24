61b8882f61dc178aaf3e1643a22464615f2931ec
sagiGL <sagigleizer@gmail.com>
2016-05-24 04:36:13 +0300

    add few customer options

--- B16 Ex03 Idan 201580990 Sagi 123456789/Ex03.ConsoleUI/GarageManager.cs
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03.ConsoleUI/GarageManager.cs
@@ -11,16 +11,28 @@ namespace Ex03.ConsoleUI
 
         private List<VehicleStatusInfo> m_VehiclesList;
         private int m_NumberOfVehicles;
+        public List<string> m_ServiceList;
 
-        public void GarageManger()
+        //constructor
+        public void GarageManger(List<string> i_ServiceList)
         {
             m_VehiclesList = new List<VehicleStatusInfo>();
             m_NumberOfVehicles = 0;
-        } 
-        //(1) to add new vehicle to the garage and to check if he allready inside.
-        public void AddVehicle(string i_LicenseNumber, string i_VehicleOwnerName, string i_VehicleOwnerPhone)
+            m_ServiceList = i_ServiceList;
+        }
+
+        //add service to the options
+        public void AddService(string i_ServiceOption)
         {
-            VehicleStatusInfo NewVehicle = new VehicleStatusInfo(i_LicenseNumber, i_VehicleOwnerName, i_LicenseNumber);
+            m_ServiceList.add(i_ServiceOption);
+        }
+        
+        //(1) to add new vehicle to the garage and to check if he allready inside.
+        public void AddVehicle(string i_LicenseNumber, string i_VehicleOwnerName, string i_VehicleOwnerPhone,GarageLogic.Vehicle i_Vehicle)
+        {
+            VehicleStatusInfo newVehicle = new VehicleStatusInfo(i_LicenseNumber, i_VehicleOwnerName, i_LicenseNumber, i_Vehicle);
+            m_VehiclesList.Add(newVehicle);
+            m_NumberOfVehicles++;
         }
         
         //(2)no filter present list of vehicles in the garage
@@ -52,6 +64,12 @@ namespace Ex03.ConsoleUI
             m_VehiclesList[IndexOfVehicle(i_LicenseNumber)].VehicleStatus = i_VehicleStatus;
         }
 
+        //(4)Fill air in wheels.
+        public void FillAir(string i_LicenseNumber)
+        {
+            m_VehiclesList[IndexOfVehicle(i_LicenseNumber)].Vehicle.GetType();
+        }
+        
         //check if vehical allready in the garage and return its index on the vehicle status list, if not return -1.
         public int IndexOfVehicle(string i_LicenseNumber)
         {
@@ -64,6 +82,36 @@ namespace Ex03.ConsoleUI
                 }
             }
             return VehicleIndexOnList;
+        }
+
+        
+
+
+        public int ServiceOptionChoose(List<string> i_ServiceList)
+        {
+            Console.WriteLine("Hello dear custumer and thanks for coming to our humble garage. \nPlease choose one of the following options:");
+            for (int service = 1; service < i_ServiceList.Count; service++)
+            {
+                Console.WriteLine(service + ") " + i_ServiceList[service]);
+            }
+            Console.WriteLine("1) Enter a new car for repairing in the garage.");
+            Console.WriteLine("2) Show List of car in the Garage.");
+            Console.WriteLine("3) Update vehicle status in the garage.");
+            Console.WriteLine("4) Fill air in the wheels.");
+            Console.WriteLine("5) Fill fuel tank.");
+            Console.WriteLine("6) Charge electrical vehicle battery.");
+            Console.WriteLine("7) Print vehicle full information.");
+
+            string inputNumStr = Console.ReadLine();
+            int inputServiceNumInt;
+            bool goodInput = int.TryParse(inputNumStr, out inputServiceNumInt);
+            while (!goodInput || inputServiceNumInt < 1 || inputServiceNumInt > i_ServiceList.Count)
+            {
+                Console.WriteLine("Input is not valid. \nPlease choose a service number between the range " + 0 + " to " + i_ServiceList.Count + ":");
+                inputNumStr = Console.ReadLine();
+                goodInput = int.TryParse(inputNumStr, out inputServiceNumInt);
+            }
+            return inputServiceNumInt;
         }
     }
 }
--- B16 Ex03 Idan 201580990 Sagi 123456789/Ex03.ConsoleUI/Program.cs
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03.ConsoleUI/Program.cs
@@ -8,10 +8,19 @@ namespace Ex03.ConsoleUI
 {
     class Program
     {
-        public void main()
+        public static void Main()
         {
             GarageManager newGarageManager = new GarageManager();
-            Console.WriteLine("Hello dear custumer and thanks for coming to our humble garage. \n Please choose one of the following options");
+            Console.WriteLine("Hello dear custumer and thanks for coming to our humble garage. \nPlease choose one of the following options:");
+            Console.WriteLine("1) Enter a new car for repairing in the garage.");
+            Console.WriteLine("2) Show List of car in the Garage.");
+            Console.WriteLine("3) Update vehicle status in the garage.");
+            Console.WriteLine("4) Fill air in the wheels.");
+            Console.WriteLine("5) Fill fuel tank.");
+            Console.WriteLine("6) Charge electrical vehicle battery.");
+            Console.WriteLine("7) Print vehicle full information.");
+            Console.ReadLine();
+
         }
     }
 }
