64c18d3d2e5ab3c3c4f0e9a8bdaf1a2326e28984
Idan.Shuraty <idanshuraty89@gmail.com>
2016-05-25 20:34:57 +0300

    merged

---
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        private float m_MinValue;
+
+        public float MinValue
+        {
+            get { return MinValue; }
+        }
+
+        private string m_Message;
+
+        public string Message
+        {
+            get { return m_Message; }
+            set { m_Message = value; }
+        }
+
+
+        private float m_MaxValue;
+
+        public float MaxValue
+        {
+            get { return m_MaxValue; }
+        }
+
+        public ValueOutOfRangeException(Exception i_InnerException, float i_MinValue, float i_MaxValue)
+            : base(string.Format("An error occured while trying enter a value that is not in the range of {0} and {1}", i_MinValue, i_MaxValue),
+            i_InnerException)
+        { }
+    }
+}
