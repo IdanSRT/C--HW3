211b524e66fea9b7611a018affef66a64aaff0fb
Idan.Shuraty <idanshuraty89@gmail.com>
2016-05-26 18:27:01 +0300

    ValueOutOfRangeException Fix

--- B16 Ex03 Idan 201580990 Sagi 123456789/Ex03GarageLogic/ValueOutOfRangeException.cs
+++ B16 Ex03 Idan 201580990 Sagi 123456789/Ex03GarageLogic/ValueOutOfRangeException.cs
@@ -9,31 +9,42 @@ namespace Ex03.GarageLogic
     public class ValueOutOfRangeException : Exception
     {
         private float m_MinValue;
+        private float m_MaxValue;
+        private string m_Message;
 
+       
+        // Constractor
+        public ValueOutOfRangeException(Exception i_InnerException, float i_MinValue, float i_MaxValue, string i_Message)
+            : base(string.Format(i_Message, i_MinValue, i_MaxValue), i_InnerException)
+        {
+            m_Message = i_Message;
+            m_MinValue = i_MinValue;
+            m_MaxValue = i_MaxValue;
+
+        }
+
+        // Default m_Message Constructor
+        public ValueOutOfRangeException(Exception i_InnerException, float i_MinValue, float i_MaxValue)
+            : base(string.Format("An error occured while trying enter a value that is not in the range of {0} and {1}", i_MinValue, i_MaxValue),
+            i_InnerException)
+        { }
+
+        // Message get / set
+        public override string Message
+        {
+            get { return m_Message; }
+        }
+
+        // MinValue get / set
         public float MinValue
         {
             get { return MinValue; }
         }
 
-        private string m_Message;
-
-        public string Message
-        {
-            get { return m_Message; }
-            set { m_Message = value; }
-        }
-
-
-        private float m_MaxValue;
-
+        // MaxValue get / set
         public float MaxValue
         {
             get { return m_MaxValue; }
         }
-
-        public ValueOutOfRangeException(Exception i_InnerException, float i_MinValue, float i_MaxValue)
-            : base(string.Format("An error occured while trying enter a value that is not in the range of {0} and {1}", i_MinValue, i_MaxValue),
-            i_InnerException)
-        { }
     }
 }
